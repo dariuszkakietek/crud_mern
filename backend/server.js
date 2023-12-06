1cd4d9f1d7b23e72e605df732c70d99174b99f7d
Dariusz <dariuszkakietek@gmail.com>
2023-12-06 18:41:40 +0100

    jwt auth

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,19 @@ if (process.env.NODE_ENV != "production") {
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectToDb = require("./config/connectToDb");
 const notesController = require("./controllers/notesController");
+const usersController = require("./controllers/usersController");
+const requireAuth = require("./middleware/requireAuth");
 
 // Create an express app
 const app = express();
 
 //Configure express app
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
 
 // Connect to database
 connectToDb();
@@ -25,6 +29,11 @@ connectToDb();
 app.get("/", (req, res) => {
   res.json({ hello: "world" });
 });
+
+app.post("/signup", usersController.signup);
+app.post("/login", usersController.login);
+app.get("/logout", usersController.logout);
+app.get("/check-auth", requireAuth, usersController.checkAuth);
 
 app.get("/notes", notesController.fetchNotes);
 app.get("/notes/:id", notesController.fetchNote);
